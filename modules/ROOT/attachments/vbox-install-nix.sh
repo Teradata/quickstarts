78a8414e35246aec46c121d4a68e59a165a45c16
Adam Tworkiewicz <adam.tworkiewicz@teradata.com>
2021-09-24 09:31:04 -0500

    improve handling of grub menu

--- modules/ROOT/attachments/vbox-install-nix.sh
+++ modules/ROOT/attachments/vbox-install-nix.sh
@@ -6,17 +6,23 @@ vboxmanage storagectl "$VM_NAME" --name "SATA Controller" --add sata --controlle
 vboxmanage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium  "$(find $DISK_DIR -name '*disk1*')"
 vboxmanage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 1 --device 0 --type hdd --medium  "$(find $DISK_DIR -name '*disk2*')"
 vboxmanage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 2 --device 0 --type hdd --medium  "$(find $DISK_DIR -name '*disk3*')"
-VBoxManage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 3 --medium emptydrive
+# this operation is necessary to work around a bug in `storageattach --type dvddrive --medium additions`
+vboxmanage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 3 --medium emptydrive
 vboxmanage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 3 --type dvddrive --medium additions
 vboxmanage modifyvm "$VM_NAME" --natpf1 "tdssh,tcp,,4422,,22"
 vboxmanage startvm "$VM_NAME" --type headless
+
+#advance through grub options to speed things up
+vboxmanage controlvm "$VM_NAME" keyboardputscancode 1c 1c
 
 n=1
 until [ "$n" -ge 10 ]
 do
   echo "Attempting to ssh into the vm. Attempt $n. This might take a minute."
+  sleep 30
   ssh -p 4422 -o StrictHostKeyChecking=no root@localhost 'mount /dev/cdrom /media/dvd; /media/dvd/VBoxLinuxAdditions.run; echo $?' && break
   n=$((n+1))
+  echo "Waiting 10 seconds before the next attempt."
   sleep 10
 done
 
@@ -25,9 +31,13 @@ vboxmanage controlvm "$VM_NAME" acpipowerbutton
 n=1
 until [ "$n" -ge 10 ]
 do
-  echo "Checking if the vm is still running. Attempt $n. This might take a few seconds."
+  echo "Checking if the vm is still running. Attempt $n. This might take a minute."
   vboxmanage showvminfo "$VM_NAME" | grep -c "running" | grep 0 && break
   n=$((n+1))
+  echo "Waiting 10 seconds before the next attempt."
   sleep 10
 done
+
 vboxmanage startvm "$VM_NAME"
+#advance through grub options to speed things up
+vboxmanage controlvm "$VM_NAME" keyboardputscancode 1c 1c
