8680e6ff2f04c9d06e97256ee21a08e570e64051
adamtwo <adamtwo@users.noreply.github.com>
2021-09-24 01:38:58 +0000

    deploy: 0bf0395821a785b685290b90ded8f31e3ba04614

--- docs/17.10/_attachments/vbox-install-nix.sh
+++ docs/17.10/_attachments/vbox-install-nix.sh
@@ -6,16 +6,15 @@ vboxmanage storagectl "$VM_NAME" --name "SATA Controller" --add sata --controlle
 vboxmanage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium  "$(find $DISK_DIR -name '*disk1*')"
 vboxmanage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 1 --device 0 --type hdd --medium  "$(find $DISK_DIR -name '*disk2*')"
 vboxmanage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 2 --device 0 --type hdd --medium  "$(find $DISK_DIR -name '*disk3*')"
-vboxmanage storagectl "$VM_NAME" --name IDE --add ide
-
-vboxmanage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 3 --type dvddrive --medium $(vboxmanage list dvds | grep UUID | cut -d" " -f 3- | sed -e 's/^[[:space:]]*//' )
+VBoxManage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 3 --medium emptydrive
+vboxmanage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 3 --type dvddrive --medium additions
 vboxmanage modifyvm "$VM_NAME" --natpf1 "tdssh,tcp,,4422,,22"
 vboxmanage startvm "$VM_NAME" --type headless
 
 n=1
 until [ "$n" -ge 10 ]
 do
-  echo "Attempting to ssh into the vm. Attempt $n"
+  echo "Attempting to ssh into the vm. Attempt $n. This might take a minute."
   ssh -p 4422 -o StrictHostKeyChecking=no root@localhost 'mount /dev/cdrom /media/dvd; /media/dvd/VBoxLinuxAdditions.run; echo $?' && break
   n=$((n+1))
   sleep 10
@@ -23,9 +22,10 @@ done
 
 vboxmanage controlvm "$VM_NAME" acpipowerbutton
 
+n=1
 until [ "$n" -ge 10 ]
 do
-  echo "Checking if the vm is still running. Attempt $n"
+  echo "Checking if the vm is still running. Attempt $n. This might take a few seconds."
   vboxmanage showvminfo "$VM_NAME" | grep -c "running" | grep 0 && break
   n=$((n+1))
   sleep 10
