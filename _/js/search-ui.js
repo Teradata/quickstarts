eab9f935e79f61b99bc04f1f22344509be2907a4
adamtwo <adamtwo@users.noreply.github.com>
2022-02-10 17:12:33 +0000

    deploy: 4e3bdb976fe6ad4beae4e757751a5f98aa2c16b3

--- _/js/search-ui.js
+++ _/js/search-ui.js
@@ -1,12 +1,18 @@
-/* eslint-env browser */
-window.antoraLunr = (function (lunr) {
-  const scriptAttrs = document.getElementById('search-script').dataset
-  const basePath = scriptAttrs.basePath
-  const pagePath = scriptAttrs.pagePath
+;(function (globalScope) {
+  /* eslint-disable no-var */
+  var config = document.getElementById('search-ui-script').dataset
+  var snippetLength = parseInt(config.snippetLength || 100, 10)
+  var siteRootPath = config.siteRootPath || ''
+  appendStylesheet(config.stylesheet)
   var searchInput = document.getElementById('search-input')
   var searchResult = document.createElement('div')
   searchResult.classList.add('search-result-dropdown-menu')
   searchInput.parentNode.appendChild(searchResult)
+
+  function appendStylesheet (href) {
+    if (!href) return
+    document.head.appendChild(Object.assign(document.createElement('link'), { rel: 'stylesheet', href: href }))
+  }
 
   function highlightText (doc, position) {
     var hits = []
@@ -20,9 +26,8 @@ window.antoraLunr = (function (lunr) {
 
     var end = start + length
     var textEnd = text.length - 1
-    var contextOffset = 15
-    var contextAfter = end + contextOffset > textEnd ? textEnd : end + contextOffset
-    var contextBefore = start - contextOffset < 0 ? 0 : start - contextOffset
+    var contextAfter = end + snippetLength > textEnd ? textEnd : end + snippetLength
+    var contextBefore = start - snippetLength < 0 ? 0 : start - snippetLength
     if (start === 0 && end === textEnd) {
       hits.push(highlightSpan)
     } else if (start === 0) {
@@ -93,7 +98,7 @@ window.antoraLunr = (function (lunr) {
     return hits
   }
 
-  function createSearchResult(result, store, searchResultDataset) {
+  function createSearchResult (result, store, searchResultDataset) {
     result.forEach(function (item) {
       var url = item.ref
       var hash
@@ -115,8 +120,7 @@ window.antoraLunr = (function (lunr) {
     var documentHit = document.createElement('div')
     documentHit.classList.add('search-result-document-hit')
     var documentHitLink = document.createElement('a')
-    var rootPath = basePath
-    documentHitLink.href = rootPath + item.ref
+    documentHitLink.href = siteRootPath + item.ref
     documentHit.appendChild(documentHitLink)
     hits.forEach(function (hit) {
       documentHitLink.appendChild(hit)
@@ -143,6 +147,11 @@ window.antoraLunr = (function (lunr) {
     return searchResultItem
   }
 
+  function clearSearchResults (reset) {
+    if (reset === true) searchInput.value = ''
+    searchResult.innerHTML = ''
+  }
+
   function search (index, text) {
     // execute an exact match search
     var result = index.search(text)
@@ -160,10 +169,7 @@ window.antoraLunr = (function (lunr) {
   }
 
   function searchIndex (index, store, text) {
-    // reset search result
-    while (searchResult.firstChild) {
-      searchResult.removeChild(searchResult.firstChild)
-    }
+    clearSearchResults(false)
     if (text.trim() === '') {
       return
     }
@@ -176,6 +182,10 @@ window.antoraLunr = (function (lunr) {
     } else {
       searchResultDataset.appendChild(createNoResult(text))
     }
+  }
+
+  function confineEvent (e) {
+    e.stopPropagation()
   }
 
   function debounce (func, wait, immediate) {
@@ -194,22 +204,26 @@ window.antoraLunr = (function (lunr) {
     }
   }
 
-  function init (data) {
-    var index = Object.assign({index: lunr.Index.load(data.index), store: data.store})
-    var search = debounce(function () {
-      searchIndex(index.index, index.store, searchInput.value)
-    }, 100)
-    searchInput.addEventListener('keydown', search)
-
-    // this is prevented in case of mousedown attached to SearchResultItem
-    searchInput.addEventListener('blur', function (e) {
-      while (searchResult.firstChild) {
-        searchResult.removeChild(searchResult.firstChild)
-      }
-    })
+  function initSearch (lunr, data) {
+    var index = Object.assign({ index: lunr.Index.load(data.index), store: data.store })
+    var debug = 'URLSearchParams' in globalScope && new URLSearchParams(globalScope.location.search).has('lunr-debug')
+    searchInput.addEventListener(
+      'keydown',
+      debounce(function (e) {
+        if (e.key === 'Escape' || e.key === 'Esc') return clearSearchResults(true)
+        try {
+          var query = searchInput.value
+          if (!query) return clearSearchResults()
+          searchIndex(index.index, index.store, searchInput.value)
+        } catch (err) {
+          if (debug) console.debug('Invalid search query: ' + query + ' (' + err.message + ')')
+        }
+      }, 100)
+    )
+    searchInput.addEventListener('click', confineEvent)
+    searchResult.addEventListener('click', confineEvent)
+    document.documentElement.addEventListener('click', clearSearchResults)
   }
 
-  return {
-    init: init,
-  }
-})(window.lunr)
+  globalScope.initSearch = initSearch
+})(typeof globalThis !== 'undefined' ? globalThis : window)
