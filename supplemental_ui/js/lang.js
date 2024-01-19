e1afa86057765f84d0e3bd11e564249ab29b8970
obed vega <vega.obed@gmail.com>
2024-01-18 19:16:44 -0800

    test redirect-3

--- supplemental_ui/js/lang.js
+++ supplemental_ui/js/lang.js
@@ -2,13 +2,13 @@ function switchLanguage(lan) {
 //    alert(lan);
     let path, page, indexFile, file, newUrl, salida;
     path = document.location.href;
-    alert('Original Path: ' + path);
+//    alert('Original Path: ' + path);
     page = path.split('/').filter(Boolean);
-    alert('page: '+page);
+//    alert('page: '+page);
     let aiUnlimited = "ai-unlimited";
     let bi = "business-intelligence";
-    alert('Components: ' + JSON.stringify(page));
-    alert(page[page.length-1]);
+//    alert('Components: ' + JSON.stringify(page));
+//    alert(page[page.length-1]);
     if(page[page.length-1]==="" || page[page.length-1]==="index.html" || page[page.length-1]==="#" || page[page.length-1]==="index.html#") {
       if(page[page.length-2] !== "ja") {
         indexFile = page.lastIndexOf(page[page.length-1]);
@@ -17,36 +17,36 @@ function switchLanguage(lan) {
         indexFile = page.lastIndexOf(page[page.length-2]);
         page.splice(indexFile, 1, '');
       }
-      alert('indexFile: '+indexFile);
+    //  alert('indexFile: '+indexFile);
     } else {
       if(lan === "en") {
         indexFile = page.lastIndexOf(page[page.length-3]);
-        alert('indexFile: ' +indexFile);
+   //     alert('indexFile: ' +indexFile);
         if (page[page.length-2] === "general"){
           file = page[page.length-1]
-          alert('file: ' +file);
+  //        alert('file: ' +file);
         } else {
           file = page[page.length-2] +"/"+ page[page.length-1];
-          alert('file: ' +file);
+   //       alert('file: ' +file);
         }
         page.splice(indexFile, indexFile+1, file);
       } else {
         if(page[page.length-3] === "es" || page[page.length-3] === "ja" ){
           page[page.length-3] = lan;  
-          alert('newUrl: ' + page[page.length-3]);
+   //       alert('newUrl: ' + page[page.length-3]);
         } else {
           if(page[page.length-3] !== "en"){
             if(page[page.length-2] === aiUnlimited || page[page.length-2] === bi){
               file = page[page.length-2]
               indexFile = page.lastIndexOf(file);
               newUrl = lan + "/"+page[page.length-2]+"/" + page[page.length-1]; 
-              alert('newUrl ja: ' +newUrl);
+      //        alert('newUrl ja: ' +newUrl);
               page.splice(indexFile, 2, newUrl)
             } else {
               file = page[page.length-1]
               indexFile = page.lastIndexOf(file);
               newUrl = lan + "/general/" + page[page.length-1]; 
-              alert('newUrl en: ' +newUrl);
+       //       alert('newUrl en: ' +newUrl);
             //  newUrl = lan +"/" + page[page.length-1]; 
               page.splice(indexFile, 1, newUrl)
             }
@@ -54,6 +54,7 @@ function switchLanguage(lan) {
         }
       }     
     }     
+    alert(page);
     salida = page.join('/');
     
     
